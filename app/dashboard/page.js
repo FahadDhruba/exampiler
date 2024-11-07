ddce66dff46d7faf800396e0b4771ef02f4a759e
Fahad Dhruba <dhruba.fahad2004@gmail.com>
2024-11-07 15:04:50 +0000

    pre done

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: "Dashboard",
+    description: "Next Gen Solution to Exam Question Creation.",
+  };
+
+export default function Dashboard() {
+  return (
+    <main>
+      Dashboard
+    </main>
+  );
+}
